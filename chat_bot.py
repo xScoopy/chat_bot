6f722b007aa4a2681608fbceaae7a579a6a95312
xScoopy <JLeary070@gmail.com>
2020-09-17 11:43:04 -0700

    welcome statements added to .py file

---
+++ chat_bot.py
@@ -0,0 +1,4 @@
+#Character customization chatbot
+print("Welcome to the Character Creator Bot!")
+print("If you're stuck on what your next character should look like, you've come to the perfect spot for inspiration.")
+print("Currently I can assist with many facial features and overall height of the character")
