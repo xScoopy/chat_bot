bfd54fe9d513de355f68de5762503cdbd09112cd
xScoopy <JLeary070@gmail.com>
2020-09-19 20:43:09 -0700

    completed lists for ears, nose, and mouth. defined get user response function

--- chat_bot.py
+++ chat_bot.py
@@ -2,3 +2,17 @@
 print("Welcome to the Character Creator Bot!")
 print("If you're stuck on what your next character should look like, you've come to the perfect spot for inspiration.")
 print("Currently I can assist with many facial features and overall height of the character")
+
+#Creation of Bot response lists:
+    #Nose list
+bot_response_nose = ["Make it long and narrow", "Short and stubby", "Crooked with one nostril", "Two dots for nostrils"]
+    #Mouth list
+bot_response_mouth = ["Skinny bird lips!", "Crooked smile", "Throw some botox in there", "Super wide like the Joker"]
+    #Ear list
+bot_response_ear = ["Big floppy ears", "Droopy earlobes", ""]
+
+
+
+#Creation of function : get_bot_response()
+get_bot_response(user_response) :
+    
